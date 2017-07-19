192bdb2416099615c4540087d8f1ad4f77762437
Nevat <dlara@openmailbox.org>
2017-07-19 12:59:31 +0200

    More variable changes. Fix some idents

--- src/drawing.c
+++ src/drawing.c
@@ -4,15 +4,12 @@
 
 void drawscreen (SDL_Renderer *renderer,uint stagedata[][22][32],SDL_Texture *tiles,uint room[],uint counter[],uint changeflag,Mix_Chunk *fx[],uint changetiles) {
 
-	int coordx = 0;
-	int coordy = 0;
 	SDL_Rect srctiles = {0,0,8,8};
 	SDL_Rect destiles = {0,0,8,8};
-	uint data = 0;
 
-	for (coordy=0; coordy<=21; coordy++) {
-		for (coordx=0; coordx<=31; coordx++) {
-			data = stagedata[room[0]][coordy][coordx];
+	for (uint8_t coordy=0; coordy<=21; coordy++) {
+		for (uint8_t coordx=0; coordx<=31; coordx++) {
+			uint16_t data = stagedata[room[0]][coordy][coordx];
 			if ((data > 0) && (data != 99)) {
 				destiles.x = coordx * 8;
 				destiles.y = coordy * 8;
@@ -118,7 +115,6 @@ void statusbar (SDL_Renderer *renderer,SDL_Texture *tiles,int room[],int lifes,i
 	SDL_Rect desnumbers = {18,178,10,10};
 	SDL_Rect srctext = {0,0,140,20};
 	SDL_Rect destext = {115,176,136,18};
-	int i = 0;
 
 	/* Show heart and crosses sprites */
 	if (changetiles == 1)
@@ -129,34 +125,34 @@ void statusbar (SDL_Renderer *renderer,SDL_Texture *tiles,int room[],int lifes,i
 	desbar.x = 32;
 	SDL_RenderCopy(renderer,tiles,&srcbar,&desbar);
 
-	for (i=0; i<=2; i++) {
+	for (uint8_t i=0; i<=2; i++) {
 		switch (i) {
-			case 0: srcnumbers.x = lifes * 10;
+			case 0: 	srcnumbers.x = lifes * 10;
+						SDL_RenderCopy(renderer,fonts,&srcnumbers,&desnumbers);
+						break;
+			case 1: 	if (crosses < 10) {
+							desnumbers.x = 50;
+							srcnumbers.x = crosses * 10;
 							SDL_RenderCopy(renderer,fonts,&srcnumbers,&desnumbers);
-							break;
-			case 1: if (crosses < 10) {
-								desnumbers.x = 50;
-								srcnumbers.x = crosses * 10;
-								SDL_RenderCopy(renderer,fonts,&srcnumbers,&desnumbers);
-							}
-							else {
-								desnumbers.x = 50;
-								srcnumbers.x = 10;
-								SDL_RenderCopy(renderer,fonts,&srcnumbers,&desnumbers);
-								desnumbers.x = 55;
-								srcnumbers.x = (crosses - 10) * 10;
-								SDL_RenderCopy(renderer,fonts,&srcnumbers,&desnumbers);
-							}
-							break;
-			case 2: if ((room[0] > 0) && (room[0] < 4)) {
-								srctext.y = (room[0] - 1) * 20;
-								SDL_RenderCopy(renderer,fonts,&srctext,&destext);
-							}
-							if (room[0] > 4) {
-								srctext.y = (room[0] - 2) * 20;
-								SDL_RenderCopy(renderer,fonts,&srctext,&destext);
-							}
-							break;
+						}
+						else {
+							desnumbers.x = 50;
+							srcnumbers.x = 10;
+							SDL_RenderCopy(renderer,fonts,&srcnumbers,&desnumbers);
+							desnumbers.x = 55;
+							srcnumbers.x = (crosses - 10) * 10;
+							SDL_RenderCopy(renderer,fonts,&srcnumbers,&desnumbers);
+						}
+						break;
+			case 2: 	if ((room[0] > 0) && (room[0] < 4)) {
+							srctext.y = (room[0] - 1) * 20;
+							SDL_RenderCopy(renderer,fonts,&srctext,&destext);
+					  	}
+						if (room[0] > 4) {
+							srctext.y = (room[0] - 2) * 20;
+							SDL_RenderCopy(renderer,fonts,&srctext,&destext);
+						}
+						break;
 		}
 
 	}
@@ -165,18 +161,15 @@ void statusbar (SDL_Renderer *renderer,SDL_Texture *tiles,int room[],int lifes,i
 
 void drawrope (struct enem enemies,SDL_Renderer *renderer,SDL_Texture *tiles,uint changetiles) {
 
-	int i = 0;
-	int blocks = 0;
-	int j = 0;
 	SDL_Rect srctile = {424,8,16,8};
 	SDL_Rect destile = {0,0,16,8};
 
-	for (i=2; i<6; i++) {
-		blocks = (enemies.y[i] - (enemies.limleft[i] - 8)) / 8;
-		for (j=0; j<=blocks; j++) {
+	for (uint8_t i=2; i<6; i++) {
+		int16_t blocks = (enemies.y[i] - (enemies.limleft[i] - 8)) / 8;
+		for (uint8_t j=0; j<=blocks; j++) {
 			srctile.y = 8 + (changetiles * 120);
-	  	destile.x = enemies.x[i];
-	  	destile.y = (enemies.limleft[i] - 8) + (8 * j);
+	  		destile.x = enemies.x[i];
+	  		destile.y = (enemies.limleft[i] - 8) + (8 * j);
 			SDL_RenderCopy(renderer,tiles,&srctile,&destile);
 		}
 	}
@@ -188,56 +181,54 @@ void drawshoots (float proyec[],SDL_Texture *tiles,SDL_Renderer *renderer,struct
 
 	SDL_Rect srctile = {656,24,16,8};
 	SDL_Rect destile = {0,0,0,0};
-	int i = 0;
-	int n = 0;
 
 	srctile.y = 24 + (changetiles * 120);
 
-  for (n=0; n<=4; n+=2) {
+	for (uint8_t n=0; n<=4; n+=2) {
 		if (proyec[n] > 0) {
-	  	i = proyec[n+1];
-	  	if (enemies->type[i] == 15) {
+	  		uint8_t i = proyec[n+1];
+	  		if (enemies->type[i] == 15) {
 				srctile.h = 16;
 				srctile.x = 640 - (16 * enemies->direction[i]);
-	  	}
+	  		}
 
-	  	/* Move shoot */
-	  	if (enemies->direction[i] == 1) {
-				if (proyec[n] > enemies->limleft[i])
-				  proyec[n] -= 2.5;
-				else {
-				  enemies->fire[i] = 0;
-				  enemies->speed[i] = 0;
-				  proyec[n] = 0;
-				}
-	  	}
-	  	else {
-				if (proyec[n] < enemies->limright[i])
-		  		proyec[n] += 2.5;
-				else {
-		  		enemies->fire[i] = 0;
-				  enemies->speed[i] = 0;
-				  proyec[n] = 0;
-				}
-	  	}
-	  	destile.w = srctile.w;
+		  	/* Move shoot */
+		  	if (enemies->direction[i] == 1) {
+					if (proyec[n] > enemies->limleft[i])
+					  proyec[n] -= 2.5;
+					else {
+					  enemies->fire[i] = 0;
+					  enemies->speed[i] = 0;
+					  proyec[n] = 0;
+					}
+	  		}
+	  		else {
+					if (proyec[n] < enemies->limright[i])
+		  			proyec[n] += 2.5;
+					else {
+		  			enemies->fire[i] = 0;
+					  enemies->speed[i] = 0;
+					  proyec[n] = 0;
+					}
+	  		}
+	  		destile.w = srctile.w;
 			destile.h = srctile.h;
 
-	  	/* Draw shoot */
-	  	switch (enemies->direction[i]) {
-				case 0: if ((proyec[n] < (enemies->limright[i] - 8)) && (proyec[n] != 0)) {
-								  destile.x = proyec[n];
-								  destile.y = enemies->y[i] + 8;
+	  		/* Draw shoot */
+	  		switch (enemies->direction[i]) {
+					case 0: 	if ((proyec[n] < (enemies->limright[i] - 8)) && (proyec[n] != 0)) {
+							  		destile.x = proyec[n];
+									destile.y = enemies->y[i] + 8;
 									SDL_RenderCopy(renderer,tiles,&srctile,&destile);
 								}
 								break;
-				case 1: if (proyec[n] > (enemies->limleft[i] + 8)) {
-								  destile.x = proyec[n];
-								  destile.y = enemies->y[i] + 8;
+					case 1: 	if (proyec[n] > (enemies->limleft[i] + 8)) {
+									destile.x = proyec[n];
+									destile.y = enemies->y[i] + 8;
 									SDL_RenderCopy(renderer,tiles,&srctile,&destile);
 								}
 								break;
-	  	}
+	  		}
 		}
 
 	}
