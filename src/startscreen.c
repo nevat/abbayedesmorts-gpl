7a02f81b2718ea803a5942c096145ffa037f1091
David Lara <dvlara@gmail.com>
2021-01-05 22:41:17 +0100

    No more constant redraw on startscreen

--- src/startscreen.c
+++ src/startscreen.c
@@ -6,6 +6,7 @@ void startscreen(SDL_Window *screen,uint8_t *state,uint8_t *grapset,uint8_t *ful
 
 	uint8_t exit = 0;
 	uint8_t musicplay = 0;
+    uint8_t change = 1;
 
 	SDL_Rect srcintro = {0,0,256,192};
 	SDL_Rect desintro = {0,0,256,192};
@@ -20,18 +21,24 @@ void startscreen(SDL_Window *screen,uint8_t *state,uint8_t *grapset,uint8_t *ful
 	Mix_Music *music = Mix_LoadMUS(DATADIR "/sounds/MainTitleN.ogg");
 
 	while (exit != 1) {
+        
+        if (change == 1) { /* Only redraw when change is made */
 
-		/* Cleaning the renderer */
-		SDL_RenderClear(renderer);
+            /* Cleaning the renderer */
+            SDL_RenderClear(renderer);
 
-		/* Put image on renderer */
-		if (*grapset == 0)
-			SDL_RenderCopy(renderer, intro, &srcintro, &desintro);
-		else
-			SDL_RenderCopy(renderer, intromd, &srcintro, &desintro);
+            /* Put image on renderer */
+            if (*grapset == 0)
+                SDL_RenderCopy(renderer, intro, &srcintro, &desintro);
+            else
+                SDL_RenderCopy(renderer, intromd, &srcintro, &desintro);
 
-		/* Flip ! */
-		SDL_RenderPresent(renderer);
+            /* Flip ! */
+            SDL_RenderPresent(renderer);
+            
+            change = 0;
+            
+        }
 
 		/* Play music if required */
 		if (musicplay == 0) {
@@ -43,12 +50,14 @@ void startscreen(SDL_Window *screen,uint8_t *state,uint8_t *grapset,uint8_t *ful
 		if ( SDL_PollEvent(&keyp) ) {
 			if (keyp.type == SDL_KEYDOWN) { /* Key pressed */
 				if (keyp.key.keysym.sym == SDLK_c) { /* Change graphic set */
+                    change = 1; /* enable redraw */
 					if (*grapset == 0)
 						*grapset = 1;
 					else
 						*grapset = 0;
 				}
 				if (keyp.key.keysym.sym == SDLK_i) { /* Show instructions */
+                    change = 1; /* enable redraw */
 					if (srcintro.y == 0)
 						srcintro.y = 192;
 					else {
@@ -82,6 +91,7 @@ void startscreen(SDL_Window *screen,uint8_t *state,uint8_t *grapset,uint8_t *ful
 					exit = 1;
 				}
 				if (keyp.jbutton.button == SELECT_JOYBUTTON) {
+                    change = 1; /* enable redraw */
 					if (*grapset == 0)
 						*grapset = 1;
 					else
@@ -95,9 +105,12 @@ void startscreen(SDL_Window *screen,uint8_t *state,uint8_t *grapset,uint8_t *ful
 			}
 
 		}
+		
 	}
 
 	/* Cleaning */
 	SDL_DestroyTexture(intro);
 	SDL_DestroyTexture(intromd);
+    Mix_FreeMusic(music);
+    
 }
