22fabef3d5abb8d84867cf9ae5b82d4795da03f6
David Lara <dvlara@gmail.com>
2025-06-26 18:14:20 +0200

    Update to version 2.0.5

--- src/main.h
+++ src/main.h
@@ -8,4 +8,4 @@ int check_data();
 void update_title(SDL_Window *screen, const char* title);
 void renderpresent(SDL_Renderer *renderer);
 
-#define GAME_TITLE "Abbaye des Morts v2.0.4"
+#define GAME_TITLE "Abbaye des Morts v2.0.5"
